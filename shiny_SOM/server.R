0dfbbf8b6389babebeea1a294d75267985d27b34
Unknown <drk.pierson@gmail.com>
2019-10-07 17:06:39 -0700

    Bug fix for Notes and QC file paths

--- shiny_SOM/server.R
+++ shiny_SOM/server.R
@@ -301,7 +301,8 @@ server <- function(input, output, session) {
     onclick("site_notes", 
             for(dir.i in 1:length(site.dir()))
               {
-              runjs(paste0("window.open('/HMGZD_notes/",site.dir()[dir.i],"_HMGZD_NOTES.pdf')"))
+              runjs(paste0("window.open('lter-som/HMGZD_notes/",site.dir()[dir.i],"_HMGZD_NOTES.pdf')"))
+              #runjs(paste0("window.open('http://google.com', '_blank')"))
               }
             )
     
@@ -309,7 +310,7 @@ server <- function(input, output, session) {
     onclick("site_qc", 
             for(dir.i in 1:length(site.dir()))
             {
-              runjs(paste0("window.open('/HMGZD_qc/",site.dir()[dir.i],"_HMGZD_QC.html')"))
+              runjs(paste0("window.open('lter-som/HMGZD_qc/",site.dir()[dir.i],"_HMGZD_QC.html')"))
             }
     )
      
