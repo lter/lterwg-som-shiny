b6a7f973ce49de5c586fa1f0496cbcd7eaefa9a9
Stevan Earl <stevan.earl@asu.edu>
2020-02-11 12:07:37 -0700

    tarball 20200211

--- shiny_SOM/global.R
+++ shiny_SOM/global.R
@@ -14,7 +14,7 @@ library(shinyjs)
 
 
 # load tarball rds
-tarball <- readRDS("somCompositeData_2019-11-23.rds")
+tarball <- readRDS("somCompositeData_2020-02-11.rds")
 
 # load control only function
 # source('ext_ftns/control_filter.R', chdir=T)
